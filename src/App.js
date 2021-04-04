1c660137d7be634081f623d50074fe84140c23f2
eymiramazan <ramazaneymir@gmail.com>
2021-04-04 16:08:28 +0300

    add checkout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import './App.css';
@@ -9,6 +9,8 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import User from "./pages/user/user.component.jsx";
+import CheckoutPage from "./pages/checkout/checkout.component.jsx";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
@@ -19,17 +21,17 @@ class App extends React.Component {
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
+            id: snapShot.id,
+            ...snapShot.data()
           })
         });
-      }else{
+      } else {
         setCurrentUser(userAuth);
       }
     })
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<User />)} />
         </Switch>
       </div>
